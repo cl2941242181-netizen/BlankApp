48022eaa46dec764fb91a12f186a09334f9afb57
chenli <12317759+chen-li2233@user.noreply.gitee.com>
2026-07-10 14:31:56 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using BlankApp.Views;
+using Prism.Ioc;
+
+namespace BlankApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
