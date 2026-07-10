da3028043c369e7e5a7d18cfe073f7fb8abd3b17
chenli <12317759+chen-li2233@user.noreply.gitee.com>
2026-07-10 19:34:04 +0800

    1.初步编写了调用百度API的翻译服务

--- Services/TranslateServices/BaiduTranslateService.cs
+++ Services/TranslateServices/BaiduTranslateService.cs
@@ -3,6 +3,8 @@ using BlankApp.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +12,63 @@ namespace BlankApp.Services.TranslateServices
 {
     internal class BaiduTranslateService : ITranslateService
     {
-        public Task<TranslateResult> TranslateAsync(string text, string formLanguage, string toLanguage)
+        // ====== 填入你在百度开放平台申请的凭证 ======
+        private const string AppId = "你的APP_ID";
+        private const string SecretKey = "你的密钥";
+        private const string ApiUrl = "https://fanyi-api.baidu.com/api/trans/vip/translate";
+
+
+        public static async Task<string> TranslateAsync(string text, string formLanguage, string toLanguage)
         {
             string salt = new Random().Next(100000, 999999).ToString();
 
-            return Task.FromResult(new TranslateResult());
+            string signStr = AppId + text + salt + SecretKey;
+            string sign = GetMd5Hash(signStr);
+
+            var postData = new Dictionary<string, string>
+        {
+            { "q", text },
+            { "from", formLanguage },
+            { "to", toLanguage },
+            { "appid", AppId },
+            { "salt", salt },
+            { "sign", sign }
+        };
+
+            using (var client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(postData);
+
+                // 4. 发送请求并获取结果
+                HttpResponseMessage response = await client.PostAsync(ApiUrl, content);
+                response.EnsureSuccessStatusCode();
+
+               var res = await response.Content.ReadAsStringAsync();
+
+
+            }
         }
+
+        /// <summary>
+        /// 计算 32 位小写 MD5
+        /// </summary>
+        private static string GetMd5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2")); // "x2" 表示 16 进制小写
+                }
+                return sb.ToString();
+            }
+        }
+
+
+
     }
 }
